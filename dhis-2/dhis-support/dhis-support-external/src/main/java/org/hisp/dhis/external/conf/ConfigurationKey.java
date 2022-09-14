eab8d514145fb7adc08492c76366b0b968a77bd0
helgeskjellevik <helge.skjellevik@gmail.com>
2022-09-14 11:11:43 +0200

    Add option to redirect to configurable endpoint if session has expired.

--- dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
+++ dhis-2/dhis-support/dhis-support-external/src/main/java/org/hisp/dhis/external/conf/ConfigurationKey.java
@@ -130,6 +130,8 @@ public enum ConfigurationKey
     AUDIT_AGGREGATE_MATRIX( "audit.aggregate", "", false ),
     OIDC_OAUTH2_LOGIN_ENABLED( "oidc.oauth2.login.enabled", Constants.OFF, false ),
     OIDC_LOGOUT_REDIRECT_URL( "oidc.logout.redirect_url", "", false ),
+
+    STANDARD_LOGOUT_REDIRECT_URL( "standard.logout.redirect_url", "", false ),
     OIDC_PROVIDER_GOOGLE_CLIENT_ID( "oidc.provider.google.client_id", "", true ),
     OIDC_PROVIDER_GOOGLE_CLIENT_SECRET( "oidc.provider.google.client_secret", "", true ),
     OIDC_PROVIDER_GOOGLE_MAPPING_CLAIM( "oidc.provider.google.mapping_claim", "", true ),
